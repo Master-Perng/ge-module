13920f20ec1b4bb7e4024fbdcbe7e9d23ae6c483
Perng <1253118536@qq.com>
2022-05-17 15:23:51 +0800

    hunter加入页数

--- api/hunter/hunter.go
+++ api/hunter/hunter.go
@@ -10,15 +10,15 @@ import (
 	"time"
 )
 
-func Query(Username string, key string, query string) (string, error) {
-	const api = "https://hunter.qianxin.com/openApi/search?username=%s&api-key=%s&search=%s&page=1&page_size=100&is_web=3&start_time=\"%d-01-01+00:00:00\"&end_time=\"%d-12-31+00:00:00\""
+func Query(Username string, page int, key string, query string) (string, error) {
+	const api = "https://hunter.qianxin.com/openApi/search?username=%s&api-key=%s&search=%s&page=%d&page_size=100&is_web=3&start_time=\"%d-01-01+00:00:00\"&end_time=\"%d-12-31+00:00:00\""
 	client := &http.Client{
 		Timeout: 5 * time.Second,
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return http.ErrUseLastResponse
 		},
 	}
-	url := fmt.Sprintf(api, Username, key, base64.StdEncoding.EncodeToString([]byte(query)), time.Now().Year()-1, time.Now().Year())
+	url := fmt.Sprintf(api, Username, key, base64.StdEncoding.EncodeToString([]byte(query)), page, time.Now().Year()-1, time.Now().Year())
 	req, err := http.NewRequest("GET", url, strings.NewReader(""))
 	if err != nil {
 		logsys.Error(err.Error())
