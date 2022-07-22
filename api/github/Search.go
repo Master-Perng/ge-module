c238d46239d1ad2a9df00b05b20f86329e5972c9
Perng <1253118536@qq.com>
2022-07-22 11:09:02 +0800

    github单独拿出jsp

--- api/github/Search.go
+++ api/github/Search.go
@@ -57,6 +57,57 @@ func SearchGithub(keyword string, token string) (string, error) {
 		logsys.Error(err.Error())
 		return "", err
 	}
+	logsys.Debug(keyword, string(result))
+	defer client.CloseIdleConnections()
+	return string(result), err
+
+}
+func SearchGithubJsp(keyword string, token string) (string, error) {
+	language := " language:jsp"
+	//创建client结果，里面是http连接的参数 ， 比如超时 https策略 代理等等
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+	reqUrl := fmt.Sprintf(githubapi, url.PathEscape(keyword+language))
+	req, err := http.NewRequest("GET", reqUrl, strings.NewReader(""))
+	req.Header.Add("Authorization", "token "+token)
+	req.Header.Add("Accept", "application/vnd.github.v3+json")
+	if err != nil {
+		logsys.Error(err.Error())
+		return "", err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		if strings.Contains(err.Error(), "Timeout") {
+			i := 0
+			for {
+				time.Sleep(2 * time.Second)
+				resp, err = client.Do(req)
+				i++
+				if err == nil {
+					break
+				}
+				if i > 5 {
+					defer client.CloseIdleConnections()
+					return "", err
+				}
+			}
+		} else {
+			defer client.CloseIdleConnections()
+			logsys.Error(err.Error())
+			return "", err
+		}
+	}
+	result, err := io.ReadAll(resp.Body)
+	if err != nil {
+		defer client.CloseIdleConnections()
+		logsys.Error(err.Error())
+		return "", err
+	}
+	logsys.Debug(keyword, string(result))
 	defer client.CloseIdleConnections()
 	return string(result), err
 
