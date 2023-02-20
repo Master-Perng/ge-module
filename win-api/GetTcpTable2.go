d76a89f3f18ada67dee0edf1fb82298474b6c062
Perng <1253118536@qq.com>
2023-02-20 17:09:49 +0800

    win-api

--- win-api/GetTcpTable2.go
+++ win-api/GetTcpTable2.go
@@ -14,3 +14,13 @@ type MIB_TCPTABLE2 struct {
 	DwNumEntries uint32
 	Table        [4096]MIB_TCPROW_OWNER_PID
 }
+
+type MIB_UDPROW_OWNER_PID struct {
+	DwLocalAddr uint32
+	DwLocalPort uint32
+}
+
+type MIB_UDPTABLE struct {
+	dwNumEntries uint32
+	MIB_UDPROW   [4096]MIB_UDPROW_OWNER_PID
+}
