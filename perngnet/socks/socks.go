f0817f35a96373c0124d624404b9a8cdda68cceb
Perng <1253118536@qq.com>
2024-04-15 16:08:13 +0800

    socks support

--- perngnet/socks/socks.go
+++ perngnet/socks/socks.go
@@ -20,3 +20,33 @@ type Socks5Header struct {
 	Destination string // Destination address (IPv4, IPv6, or domain name)
 	Port        uint16 // Destination port
 }
+
+func (s5 *Socks5Header) Check() bool {
+	if s5.Version != 0x05 {
+		return false
+	}
+	if s5.Command < 0x01 || s5.Command > 0x03 {
+		return false
+	}
+	if s5.Reserved != 0 {
+		return false
+	}
+	if s5.AddressType != 0x01 && s5.AddressType != 0x03 && s5.AddressType != 0x04 {
+		return false
+	}
+	return true
+
+}
+
+func (s4 *Socks4AHeader) Check() bool {
+	if s4.Version != 0x04 {
+		return false
+	}
+	if s4.Command != 0x01 && s4.Command != 0x02 {
+		return false
+	}
+	if s4.NullByte != 0x00 {
+		return false
+	}
+	return true
+}
